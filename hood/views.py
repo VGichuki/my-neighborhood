971b4562c490f9106449bb57106d526a90addeae
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-26 22:51:08 +0300

    create a view function for the single-hood

--- hood/views.py
+++ hood/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Neighborhood,Profile,Business,Post
-from .forms import CreateHoodForm
-from django.views.generic.base import View
+from .forms import CreateHoodForm,HoodBusinessForm, PostForm
+# from django.views.generic.edit import DeleteView
+# from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -22,6 +24,46 @@ def create_hood(request):
     else:
         form = CreateHoodForm()
     return render(request, 'hood.html', {'form': form})
+
+def single_hood(request,id):
+    hood = Neighborhood.objects.get(id=id)
+    if request.method =='POST':
+        form = HoodBusinessForm(request.POST)
+        if form.is_valid():
+            business_form = form.save(commit=False)
+            business_form.neighborhood = hood
+            business_form.user = request.user.profile
+            business_form.save()
+        return redirect('index')
+    else:
+        form = HoodBusinessForm()
+    return render(request, 'business.html', {'form': form})
+
+def create_post(request, hood_id):
+    hood = Neighborhood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single-hood', hood_id)
+    else:
+        form = PostForm()
+    return render(request, 'postform.html', {'form': form})
+
+def hood_members(request, hood_id):
+    hood = Neighborhood.objects.get(id=hood_id)
+    members = Profile.objects.filter(neighborhood=hood)
+    return render(request, 'members.html', {'members': members})
+
+# class DeleteHoodView(DeleteView):
+#     model = Neighborhood
+#     template_name = 'delete_hood.html'
+
+
+
 
 
 
