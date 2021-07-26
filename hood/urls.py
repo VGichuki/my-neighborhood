b84711ff44e683744d407b2d70a027b401ed56ab
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-26 22:50:29 +0300

    create a path for the single hood

--- hood/urls.py
+++ hood/urls.py
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',views.index, name ='index'),
     path('new-hood/', views.create_hood, name='new-hood'),
-    
+    path('single-hood/<int:id>/', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
