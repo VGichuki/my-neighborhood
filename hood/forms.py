d42d5461abd524b14a22005022ff2898bc648b9d
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-27 08:01:06 +0300

    create a userprofile form

--- hood/forms.py
+++ hood/forms.py
@@ -5,6 +5,8 @@ from pyuploadcare.dj.forms import ImageField
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from hood import models
+
 
 class CreateHoodForm(forms.ModelForm):
     class Meta:
@@ -25,3 +27,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighborhood')
