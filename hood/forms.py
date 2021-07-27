88fcfee80041e24e54ee50dbb813ec390b281eaa
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-27 06:28:34 +0300

    create a register form

--- hood/forms.py
+++ hood/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+import django
 from .models import Neighborhood,Profile,Business,Post
 from pyuploadcare.dj.forms import ImageField
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class CreateHoodForm(forms.ModelForm):
     class Meta:
@@ -16,3 +20,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('user', 'hood')
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
